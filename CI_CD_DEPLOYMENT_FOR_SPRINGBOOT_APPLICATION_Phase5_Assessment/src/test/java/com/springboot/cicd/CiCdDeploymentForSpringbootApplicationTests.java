67c6e6c499b1bbffa78ba5bbffbb28c0a608a1d0
Vinita <zugarevinita@gmail.com>
2022-03-27 15:34:24 +0530

    commit

--- CI_CD_DEPLOYMENT_FOR_SPRINGBOOT_APPLICATION_Phase5_Assessment/src/test/java/com/springboot/cicd/CiCdDeploymentForSpringbootApplicationTests.java
+++ CI_CD_DEPLOYMENT_FOR_SPRINGBOOT_APPLICATION_Phase5_Assessment/src/test/java/com/springboot/cicd/CiCdDeploymentForSpringbootApplicationTests.java
@@ -1,9 +1,10 @@
-package com.springboot.aws;
+package com.springboot.cicd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 @SpringBootTest
-class DeploySpringbootOnCloudApplicationTests {
+class CiCdDeploymentForSpringbootApplicationTests {
 
 	@Test
 	void contextLoads() {
